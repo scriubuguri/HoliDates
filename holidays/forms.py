6a4b594a7132f7de31b72368ec20b0ee0ea11d43
scriubuguri <scriubuguri@tragdate.ninja>
2024-04-06 08:34:08 +0000

    update views

--- holidays/forms.py
+++ holidays/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class HolidayForms(forms.Form):
-    day = forms.IntegerField(label='Day', min_value=1, max_value=31)
-    month = forms.IntegerField(label='Month', min_value=1, max_value=12)
-    year = forms.IntegerField(label='Year', min_value=1900, max_value=2100)
-    country = forms.CharField(label='Country', max_length=2)
+    Day = forms.IntegerField(label='Day', min_value=1, max_value=31)
+    Month = forms.IntegerField(label='Month', min_value=1, max_value=12)
+    Year = forms.IntegerField(label='Year', min_value=1900, max_value=2100)
+    Country = forms.CharField(label='Country', max_length=2)
--- holidays/views.py
+++ holidays/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import HolidayForms
 import requests
 import json
+
+def holi_finder(request):
+    forms = HolidayForms()
+    if request.POST:
+        data = request.POST.dict()
+        request.session['your_data'] = data
+        return redirect('holidays_result')
+    return render(request, 'index.html', {'forms': forms})
+
 
 def holidays_result(request):
     main_url = "https://holidays.abstractapi.com/v1/"
     your_api_key = ""
-    your_country = ""
-    your_year = ""
-    your_month = ""
-    your_day = ""
+    data = request.session['your_data']
+    your_country = data['Country']
+    your_year = data['Year']
+    your_month = data['Month']
+    your_day = data['Day']
 
     full_url = "{url}?{api_key}&{country}&{year}&{month}&{day}".format(
             url=main_url,
@@ -22,12 +33,13 @@ def holidays_result(request):
 
     response = requests.get(full_url)
     api_response = response.json
+    the_holiday = api_response['name']
     final_resp = "Today, {day}{month}{year}, is the holiday {holiday} in {country}.".format(
-            day='',
-            month='',
-            year='',
-            holiday='',
-            country=''
+            day=your_day,
+            month=your_month,
+            year=your_year,
+            holiday=the_holiday,
+            country=your_country
             )
 
     return HttpResponse(final_resp)
